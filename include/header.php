d4c971850f58c10bffe0fc91698c08a6f3f6f29b
Arsalan Ahmed <arsalanahmeddev1@gmail.com>
2024-04-20 14:19:26 +0500

    boiler plate changes

--- include/header.php
+++ include/header.php
@@ -1,21 +1,18 @@
-<header class="header">
-    <div class="row">
-        <div class="col-md-6">
-            <!-- <div class="logo">
-                <img src="./assets/images/logo.png" alt="">
-            </div> -->
-            <h1>hellow world 1</h1>
+<header class="">
+    <div class="top-bar pt-15 pb-15">
+        <div class="container">
+            <h3 class="topbar-para">
+            <img src="./assets/images/speaker.svg" alt="">  
+            <span class="text-white ps-3 fs-18">Important Update: Flu Vaccine Now Available for All Patients</span></h3>
         </div>
-        <div class="col-md-6">
-            <!-- <nav>
-            <ul class="primary-nav">
-                <li><a href="javascript:;"></a></li>
-                <li><a href="javascript:;"></a></li>
-                <li><a href="javascript:;"></a></li>
-                <li><a href="javascript:;"></a></li>
-            </ul>
-        </nav> -->
-            <h1>hellow world 2</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+
+            </div>
+            <div class="col-md-6">
+            </div>
         </div>
     </div>
 </header>
